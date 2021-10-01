27384b93e0b450c8f0ac713f623e6708a7417827
hugofolloni <hugofollogua07@gmail.com>
2021-10-01 17:19:27 -0300

    :rocket: First steps - adding home and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Home />
     </div>
   );
 }
