0561b0e5db1abe7accfa02461196e09522015fb9
hugofolloni <hugofollogua07@gmail.com>
2022-08-09 17:28:01 -0300

    :wrench: Fixing portuguese

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -5,7 +5,7 @@ const Aboutme = () => {
                 <h2>SOBRE MIM</h2>
             </div>
             <div className="infos-about-me">
-                <p>Olá, eu sou Hugo Folloni Guarilha e sou um desenvolvedor fullstack de 19 anos. Amo tecnologias e desenvolvimento de ideias desde a minha infância, mas em 2017 comecei a programar. Meus primeiros aplicativos foram em Python, ainda no ensino médio. Depois disso, comecei a focar no desenvolvimento web, com Javascript e HTML/CSS. Neste último ano, me aperfeiçoei meus conhecimentos em React e Node.js, e hoje domino tais ferramentas.</p>
+                <p>Olá, eu sou Hugo Folloni Guarilha e sou um desenvolvedor fullstack de 19 anos. Amo tecnologias e desenvolvimento de ideias desde a minha infância, mas em 2017 comecei a programar. Meus primeiros aplicativos foram em Python, ainda no ensino médio. Depois disso, comecei a focar no desenvolvimento web, com Javascript e HTML/CSS. Neste último ano, aperfeiçoei meus conhecimentos em React e Node.js, e hoje domino tais ferramentas.</p>
                 <br />
                 <p>Sou programador autodidata, mas agora curso a faculdade de Ciência da Computação na UFRJ, uma das universidades mais prestigiadas do país, na qual estou envolvido no projeto de ensino de programação <a href="https://github.com/hugofolloni/machine-teaching" target='_blank'  rel="noreferrer">Machine Teaching</a>. Minha perseverança e habilidade de aprender sozinho são minhas características mais marcantes, e vão me manter focado em me desenvolver como profissional.</p>
             </div>
