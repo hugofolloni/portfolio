bf423902efcb54e5b1521bcb636a65db86860827
hugofolloni <hugofollogua07@gmail.com>
2024-04-28 01:39:21 -0300

    Adding more projects

--- src/Projects.js
+++ src/Projects.js
@@ -2,8 +2,12 @@ import { Fade } from "react-awesome-reveal";
 import { useEffect, useRef, useState } from "react"
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LaunchIcon from '@mui/icons-material/Launch';
-import wedding from './assets/wedding.png'
 import styled, {keyframes} from "styled-components";
+
+import wedding from './assets/wedding.png'
+import stars from './assets/stars.png'
+import wiki from './assets/wiki.png'
+import chess from './assets/chess.png'
 
 const Projects = () => {
 
@@ -21,6 +25,51 @@ const Projects = () => {
                 'NodeJS',
                 'Express'
             ]
+        },
+        {
+            title: 'react-stars-particles',
+            photo: stars,
+            description: 'a captivating React component that brings a dynamic and interactive galaxy background to your web pages. With stars that are attracted to the mouse pointer and smooth animations, it offers a unique and engaging user experience.',
+            github: 'https://github.com/hugofolloni/react-stars-particles',
+            link: 'https://galaxysimulator.netlify.app',
+            tools: [
+                'JavaScript',
+                'React',
+                'NodeJS',
+                'Rollup'
+            ]
+        },
+        {
+            title: 'wiki-analysis',
+            photo: wiki,
+            description: 'a final project for Algorithmic Linear Algebra. It aims to categorize Wikipedia articles based on their URL and similar articles using linear algebra comparisons. The project utilizes cosine similarity between article vectors to determine categories and improves efficiency by learning from requests.',
+            github: 'https://github.com/hugofolloni/wiki-analysis',
+            link: 'https://wiki-analysis.netlify.app',
+            tools: [
+                'TypeScript',
+                'React',
+                'NodeTS',
+                'Python',
+                'PostgreSQL',
+                'BeautifulSoup',
+                "NumPy",
+                "Express"
+            ]
+        },
+        {
+            title: 'stockglass',
+            photo: chess,
+            description: 'implementation of a chess game with Pygame, featuring classes for the game, pieces, and the board. It utilizes algorithms to make computer moves. The game logic manages moves, checks, and interactions, while the graphical interface displays the board and allows interactive gameplay.',
+            github: 'https://github.com/hugofolloni/wiki-analysis',
+            link: '',
+            tools: [
+                'Python',
+                'Pygame',
+                'Numpy',
+                'Python',
+                'Pygame',
+                'Numpy'
+            ]
         }
     ]
 
@@ -37,7 +86,7 @@ const Projects = () => {
                                             &&
                                         <LeftProject item={item}/>)
                                             || 
-                                        <div className=""></div>
+                                        <RightProject item={item}/>
                                     }
                                 </div>
                             ))
@@ -72,22 +121,22 @@ const LeftProject = (props) => {
         100% { transform: translateX(-${size}px)}
     `
 
-
+    const [speed, setSpeed] = useState(10)
     const SlideTrack = styled.div`
-        animation: ${scroll} 10s linear infinite; 
+        animation: ${scroll} ${speed}s linear infinite; 
         display: flex;
         width: 100%;
         flex-direction: row;
     `       
 
-
     useEffect(() => {
-        var size = 20;
+        var size = 15;
         for(let i = 0; i < ref.current.children.length / 2; i++){
             size += ref.current.children[i].offsetWidth
-            size += 5
+            size += 7
         }
         setSize(size)
+        setSpeed(ref.current.children.length)
     }, [])
 
     const item = props.item;
@@ -101,10 +150,10 @@ const LeftProject = (props) => {
                     <span>{item.description}</span>
                 </div>
                 <div className="left-project-links">
-                    <a href={item.link}>
+                    {item.link !== "" && <a href={item.link} target='_blank' rel='noreferrer'>
                         <LaunchIcon className='icon'/>          
-                    </a>
-                    <a href={item.github}>
+                    </a>}
+                    <a href={item.github} target='_blank' rel='noreferrer'>
                         <GitHubIcon className='icon'/>          
                     </a>
                 </div>
@@ -120,6 +169,80 @@ const LeftProject = (props) => {
                 </div>
             </div>
         </Fade>
+    )
+}
+
+const RightProject = (props) => {
+    const ref = useRef(null)
+
+    const Tool = styled.div`  
+        background-color:#110033;
+        color: white;
+        margin: 5px;
+        border-radius: 6px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        padding: 10px;
+        font-size: 16px;
+        `
+        
+    const [size, setSize] = useState(0)
+    const scroll = keyframes`
+        0% { transform: translateX(0px); }
+        100% { transform: translateX(${size}px)}
+    `
+
+
+    const [speed, setSpeed] = useState(10)
+    const SlideTrack = styled.div`
+        animation: ${scroll} ${speed}s linear infinite; 
+        display: flex;
+        width: 100%;
+        flex-direction: row-reverse;
+    `       
+
+    useEffect(() => {
+        var size = 15;
+        for(let i = 0; i < ref.current.children.length / 2; i++){
+            size += ref.current.children[i].offsetWidth
+            size += 7
+        }
+        setSize(size)
+        setSpeed(ref.current.children.length)
+    }, [])
+
+    const item = props.item;
+    console.log(item)
+    return (
+        <Fade style={{width: '100%', height:'100%'}} direction='left' distance={"30px"}> 
+            <div className="right-project">
+                <div className="project-image-wrapper"><img src={item.photo} alt="" /><div className="grainy"/></div>
+                <div className="right-project-description">
+                    <div className="orange">{item.title}</div>
+                    <span>{item.description}</span>
+                </div>
+                <div className="right-project-links">
+                   {item.link !== "" && <a href={item.link} target='_blank' rel='noreferrer'>
+                        <LaunchIcon className='icon'/>          
+                    </a>}
+                    <a href={item.github} target='_blank' rel='noreferrer'>
+                        <GitHubIcon className='icon'/>          
+                    </a>
+                </div>
+                <div className="right-project-tools">
+                    <SlideTrack ref={ref}>
+                        {item.tools.map((item) => (
+                            <Tool>{item}</Tool>
+                        ))}
+                        {item.tools.map((item) => (
+                            <Tool>{item}</Tool>
+                        ))}
+                    </SlideTrack>
+                </div>
+            </div>
+    </Fade> 
     )
 }
  
