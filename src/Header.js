e0a6a9a2a46a104dc619bd08f55b95c27b31aa4b
hugofolloni <hugofollogua07@gmail.com>
2025-02-19 19:40:33 -0300

    fix: replace wrong URLs to other social medias

--- src/Header.js
+++ src/Header.js
@@ -10,10 +10,10 @@ const Header = () => {
             <span onClick={() => {window.scrollTo(0, 0)}}>hugofolloni</span>
           </div>
           <div className="links">
-            <a href="https://www.linkedin.com/in/hugofolloni/" target="_blank" rel='noreferrer'>
+            <a href="https://www.linkedin.com/in/folloni/" target="_blank" rel='noreferrer'>
               <LinkedInIcon className='icon'/>          
             </a>
-            <a href="https://github.com/folloni/" target="_blank" rel='noreferrer'>
+            <a href="https://github.com/hugofolloni/" target="_blank" rel='noreferrer'>
               <GitHubIcon className='icon'/>          
             </a>
             <a href="/resume.pdf" target="_blank" rel='noreferrer'>
